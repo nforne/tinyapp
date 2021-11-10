77fa7a00f3142b0589d229cb82845d9d443020d3
nforne <mnforne@gmail.com>
2021-11-09 22:50:07 -0800

    add POST route, generateRandomString() and body-parser

--- express_server.js
+++ express_server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const alphanumeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let key = "";
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * 36);
+    key += alphanumeric[index];
+  }
+  return key;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -20,6 +33,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
