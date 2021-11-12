c0b2f62cdb8978337b41b773398ad162f2460def
nforne <mnforne@gmail.com>
2021-11-12 06:49:18 -0800

    debug refactor and cleanup for readability and simplicity

--- express_server.js
+++ express_server.js
@@ -33,7 +33,7 @@ const users = {
   }
 }
 
-const usersdbIDs = Object.keys(users);
+// const usersdbIDs = Object.keys(users);
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -53,18 +53,36 @@ const urlDatabase = {
   }
 };
 
-const urlDBKeys = Object.keys(urlDatabase);
+// const urlDBKeys = Object.keys(urlDatabase);
 
-//-------------------------------- 
-let flatUrlDB = {};
-for (let i of urlDBKeys) {
-  outPut[i] = urlDatabase[i]['longURL'];
+//--------------------------------------
+const flatUrlDB = () => {
+  const urlDBKeys = Object.keys(urlDatabase);
+  let outPut = {};
+  for (let i of urlDBKeys) {
+    outPut[i] = urlDatabase[i]['longURL'];
+  }
+  return outPut;
 }
-//-------------------------------
+
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-let loginID = ''; // for granting prevelege to other site resources
+let loginID = ''; // for granting access to other site resources
+
+//--------------------------------------
+
+const userID = () => {
+  const usersdbIDs = Object.keys(users);
+  let userID = '';
+  for (let i of usersdbIDs) {
+    if (users[i]['email'] === loginID[0]) {
+      userID = i;
+    }
+  }
+  return userID;
+}
+console.log(userID());
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -82,6 +100,7 @@ function generateRandomString(n) {
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 const emailCheck = (email) => {
+  const usersdbIDs = Object.keys(users);
   let outPut = false;
   for (let i of usersdbIDs) {
     if (users[i]['email'] === email) {
@@ -100,14 +119,13 @@ app.get("/", (req, res) => {
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 app.get("/register", (req, res) => {
-  const templateVars = { urls: {}, username:''};
+  const templateVars = { urls: {}, email:''};
   res.render('user_registrationf', templateVars);
 });
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-app.post("/register", (req, res) => {
-  // console.log(req.body.email.length);
+app.post("/register", (req, res) => {  
 
   if (req.body.email.length === 0 || req.body.password.length === 0) {
     res.send("<html><body>404 \n<b>Email or password field empty. Not optional, must be filled</b></body></html>\n");
@@ -116,39 +134,42 @@ app.post("/register", (req, res) => {
     res.send("<html><body>404 \n<b>Email already in use by someone else! Try another.</b></body></html>\n");
     
   } else {
+    let cookieValue = '';
+    const usersdbIDs = Object.keys(users);
     while (true) { // check to make sure there is no userID duplication at auto generate
       let randomID = generateRandomString(4);
       if (!usersdbIDs.includes(randomID)) {      
+        cookieValue = randomID;
         let bodyjson = req.body;
         bodyjson['id'] = randomID;
         users[randomID] = bodyjson
         loginID = [req.body.email];
-        console.log(users)
+        // console.log(users)
         break;
       }
     }
-    const templateVars = { urls: flatUrlDB, username: loginID[0]};
-    res.cookie('username', req.body.username /*, {httpOnly:true}*/);
-    res.render('urls_index.ejs', templateVars);
+    const templateVars = { urls: flatUrlDB(), email: loginID[0]};
+    res.cookie('user_id', cookieValue /*, {httpOnly:true}*/);
+    res.render('urls_index', templateVars);
   }
 });
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-app.post("/login", (req, res) => {
-  console.log(req.body)
-  console.log(users);
+app.post("/login", (req, res) => {  
 
   if (req.body.email && req.body.password) {
+    const usersdbIDs = Object.keys(users);
     for (let i of usersdbIDs) {
       if (users[i]['email'] === String(req.body.email) && users[i]['password'] === String(req.body.password)) {
 
         loginID = [users[i]['email']];        
 
-        res.cookie('user_id', users[i]['id'] /*, {httpOnly:true}*/);
+        res.cookie('user_id', users[i]['id'] /*, {httpOnly:true}*/);        
+        
         const templateVars = {
-          username: req.body.username,
-          urls: flatUrlDB // urlDatabase
+          email: req.body.email,
+          urls: flatUrlDB() // urlDatabase
         // ... any other vars
         };
         res.render("urls_index", templateVars);
@@ -173,8 +194,8 @@ app.get("/login", (req, res) => {
 
 app.get("/urls", (req, res) => {
   if (loginID[0]) {    
-    const templateVars = { urls: flatUrlDB, username: loginID[0]};
-    res.render("urls_index.ejs", templateVars);
+    const templateVars = { urls: flatUrlDB(), email: loginID[0]};
+    res.render("urls_index", templateVars);
   } else {
     res.render("urls_login",);
   }
@@ -185,8 +206,8 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   if (loginID[0]) {    
     const templateVars = {
-      username: loginID[0],
-      urls: flatUrlDB
+      email: loginID[0],
+      urls: flatUrlDB(),
       // ... any other vars
     };
     res.render("urls_new", templateVars);
@@ -198,21 +219,15 @@ app.get("/urls/new", (req, res) => {
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 app.post("/urls", (req, res) => {
-  let urlDBKeys = Object.keys(urlDatabase);
-  while (true) { // check to make sure there is no shortURL duplication
+  
+  const urlDBKeys = Object.keys(urlDatabase);
+  while (true) {      // to make sure there is no shortURL duplication
     let shortURL = generateRandomString(6);
-    //--------------------------------------
-    let userID = '',
-    for (let i of usersdbIDs) {
-      if (users[i]['email'] === loginID[0]) {
-        userID = i;
-      }
-    }
-    //-------------------------------------
     if (!urlDBKeys.includes(shortURL)) {
-      urlDatabase[shortURL] = {'longURL' : req.body.longURL, 'userID' : userID };
+      urlDatabase[shortURL] = {'longURL': req.body.longURL, 'userID': userID()};
+      // console.log(urlDatabase);
+
       res.redirect(`/urls/${shortURL}`);
-      console.log(urlDatabase);
       break;
     }
   }
@@ -220,20 +235,24 @@ app.post("/urls", (req, res) => {
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-app.post('/urls/:shortURL/update', (req, res) => {
-  let urlDBKeys = Object.keys(urlDatabase);
-  let longURL = [String(urlDatabase[req.params.shortURL])];
+app.post('/urls/:shortURL/update', (req, res) => { 
+
+  const urlDBKeys = Object.keys(urlDatabase);
+  let longURL = [flatUrlDB()[req.params.shortURL]];  
   for (let i of urlDBKeys) {
-    if (urlDatabase[i] === longURL[0]) {
+    if (urlDatabase[i]['longURL'] === longURL[0]) {
       delete urlDatabase[i];
     }
   }
+  
   while (true) { // check to make sure there is no shortURL duplication
     let shortURL = generateRandomString(6);
-    if (!urlDBKeys.includes(shortURL)) {
-      urlDatabase[shortURL] = longURL[0];
+    
+    if (!urlDBKeys.includes(shortURL)) {    
+      
+      urlDatabase[shortURL] = {'longURL':longURL[0],'userID': userID()};      
+     
       res.redirect(`/urls/${shortURL}`);
-      // console.log(urlDatabase);
       break;
     }
   }
@@ -249,7 +268,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL]['longURL'];
   res.redirect(longURL);
 });
 
@@ -257,9 +276,9 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   if (loginID[0]) {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: loginID[0],};
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]['longURL'], email: loginID[0],};
     res.render("urls_show", templateVars);
-  }else {
+  } else {
     res.render("urls_login",);
   } 
 });
