720343bf9a5fb98322985c1d5dd8ebcafc7e8465
nforne <mnforne@gmail.com>
2021-11-22 01:25:32 -0800

    cleanup and refactor

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const bcrypt = require('bcryptjs');
 const cookieSession = require('cookie-session')
+
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
@@ -16,11 +17,11 @@ app.use(cookieSession({
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))  // override with POST having ?_method=DELETE
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.set("view engine", "ejs");
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 const users = { 
   "user1RandomID": {
@@ -43,7 +44,7 @@ const users = {
   }
 }
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 const urlDatabase = {
   b6UTxQ: {
@@ -69,9 +70,9 @@ const sampleUrlDB = { // for demo on the front page Tinyapp
   }
 };
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
-let loginID = []; // for granting access to other site resources
+let loginID = []; // for granting access to other site resources by signInCheck
 
 const signInCheck = (req) => {
   if (req.session.user_id && loginID.includes(req.session.user_id)) {
@@ -81,79 +82,32 @@ const signInCheck = (req) => {
   }
 }
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
-const {flatUrlDB, generateRandomString} = require('./helpers');
+const {flatUrlDB, generateRandomString, emailCheck, fetchEmailById, querry_DB_By_ID } = require('./helpers');
 
-//--------------------------------------
-const emailCheck = (email) => {
-  const usersdbIDs = Object.keys(users);
-  let outPut = false;
-  for (let i of usersdbIDs) {
-    if (users[i]['email'] === email) {
-      outPut = true;
-    } 
-  }
-  return outPut;
-};
-
-//--------------------------------------
-
-const fetchEmailById = (id) => {
-  const usersdbIDs = Object.keys(users);
-  for (let i of usersdbIDs) {
-    if (users[i]['id'] === id) {
-      return users[i]['email'];
-    }
-  }
-}
-//--------------------------------------
-
-const querry_DB_By_ID = (id) => { // to retrieve only data of a particular user id
-  let outPut = {} 
-  const urlDBKeys = Object.keys(urlDatabase);
-  for (let i of urlDBKeys) {
-    if (urlDatabase[i]['userID'] === id) {
-      outPut[i] = urlDatabase[i];
-    }
-  }
-  return flatUrlDB(outPut);
-};
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-app.get("/", (req, res) => {
-  const urlDBKeys = Object.keys(sampleUrlDB);
-  let outPut = {};
-  for (let i of urlDBKeys) {
-    outPut[i] = sampleUrlDB[i]['longURL'];
-  }
-  const templateVars = { urls: outPut};
-  res.render("urls_indexPublic", templateVars);
-});
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.get("/register", (req, res) => {
-  const templateVars = { urls: {}, email:''};
+  const templateVars = { urls: {}, email:null};
   res.render('user_registrationf', templateVars);
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.post("/register", (req, res) => {  
 
   if (req.body.email.length === 0 || req.body.password.length === 0) {
     res.send("<html><body>404 \n<b>Email or password field empty. Not optional, must be filled</b></body></html>\n");
     
-  } else if (emailCheck(req.body.email)) {
+  } else if (emailCheck(req.body.email, users)) {
     res.send("<html><body>404 \n<b>Email already in use by someone else! Try another.</b></body></html>\n");
     
   } else {
     let cookieValue = '';
     const usersdbIDs = Object.keys(users);
 
-    while (true) { // check to make sure there is no userID duplication at auto generate
+    while (true) { // a check to make sure there is no userID duplication at auto generate
       let randomID = generateRandomString(4);
       if (!usersdbIDs.includes(randomID)) {       
         let bodyjson = req.body;
@@ -165,13 +119,13 @@ app.post("/register", (req, res) => {
         break;
       }
     }
-    const templateVars = { urls: querry_DB_By_ID(cookieValue), email: req.body.email};
+    const templateVars = { urls: querry_DB_By_ID(cookieValue, urlDatabase), email: req.body.email};
     req.session.user_id = cookieValue;
     res.render('urls_index', templateVars);
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.post("/login", (req, res) => {  
 
@@ -185,8 +139,8 @@ app.post("/login", (req, res) => {
         req.session.user_id = users[i]['id'];
         const templateVars = {
           email: req.body.email,
-          urls: querry_DB_By_ID(req.session.user_id)        
-        };
+          urls: querry_DB_By_ID(req.session.user_id, urlDatabase)      
+        }
         res.render("urls_index", templateVars);
       }
      }
@@ -195,53 +149,55 @@ app.post("/login", (req, res) => {
       req.session = null;
       res.render("urls_login");
   } else {
-    res.render("urls_login");
+    res.render('urls_alert');
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.get("/login", (req, res) => {
-  res.render("urls_login");
+  const templateVars = {email: null};
+  res.render("urls_login", templateVars);
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
+
+app.get("/", (req, res) => {
+  if (signInCheck(req)) {
+    const templateVars = { urls: querry_DB_By_ID(req.session.user_id, urlDatabase), email: fetchEmailById(req.session.user_id, users)};
+    res.render("urls_index", templateVars);
+  } else {
+    res.render('urls_alert');       
+  }
+  
+});
+
+//--------------------------------------------------------------------------
 
 app.get("/urls", (req, res) => {  
   if (signInCheck(req)) {
-    const templateVars = { urls: querry_DB_By_ID(req.session.user_id), email: fetchEmailById(req.session.user_id)};
+    const templateVars = { urls: querry_DB_By_ID(req.session.user_id, urlDatabase), email: fetchEmailById(req.session.user_id, users)};
     res.render("urls_index", templateVars);
-  } else {
-    res.render("urls_login",);
+  } else {    
+    res.render('urls_alert')
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.get("/urls/new", (req, res) => {
   if (signInCheck(req)) {    
     const templateVars = {
-      email: fetchEmailById(req.session.user_id),
-      urls: flatUrlDB(urlDatabase)
+      email: fetchEmailById(req.session.user_id, users),
+      urls: flatUrlDB(urlDatabase)  //----------------------------------------------------------
     };
     res.render("urls_new", templateVars);
   } else {
-    res.render("urls_login",);
+    res.render('urls_alert')
   }
 });
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-app.get ('/public', (req, res) => {  // demo view
-  const urlDBKeys = Object.keys(sampleUrlDB);
-  let outPut = {};
-  for (let i of urlDBKeys) {
-    outPut[i] = sampleUrlDB[i]['longURL'];
-  }
-  const templateVars = { urls: outPut};
-    res.render("urls_indexPublic", templateVars);
-}) 
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.post("/urls", (req, res) => {
   if (signInCheck(req) && req.body.longURL.length !== 0) {
@@ -259,14 +215,14 @@ app.post("/urls", (req, res) => {
   } else if (signInCheck(req)) {
 
   } else {
-    res.render("urls_login");
+    res.render('urls_alert');
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.put('/urls/:shortURL/update', (req, res) => { 
-  if (signInCheck(req)) {
+  if (signInCheck(req) && req.session.user_id === urlDatabase[req.params.shortURL]['userID']) { // only owners have create update and delete powers
 
     const urlDBKeys = Object.keys(urlDatabase);
     let longURL = [flatUrlDB(urlDatabase)[req.params.shortURL]];  
@@ -288,67 +244,86 @@ app.put('/urls/:shortURL/update', (req, res) => {
       }
     }
   } else {
-    res.render("urls_login",);
+    res.render('urls_alert');
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-app.post('/urlUseCount', (req, res) => {
-  if (signInCheck(req) && req.body.shortURL) { //-------------------------to track url use for analytics-------------------------------
-    urlDatabase[req.body.shortURL]['urlUseCount'] += 1;
-  } else {
-    res.render("urls_login",);
-  }
-})
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.delete('/urls/:shortURL/delete', (req, res) => {
   if (signInCheck(req)) {
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
   } else {
-    res.render("urls_login",);
+    res.render('urls_alert');
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.get("/u/:shortURL", (req, res) => {
   if (signInCheck(req)) {    
     const longURL = urlDatabase[req.params.shortURL]['longURL'];
     res.redirect(longURL);
   } else {
-    res.render("urls_login",);
+    res.render('urls_alert');
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.get("/urls/:shortURL", (req, res) => {
-  if (signInCheck(req)) {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]['longURL'], email: fetchEmailById(req.session.user_id)};
+  if (signInCheck(req) && req.session.user_id === urlDatabase[req.params.shortURL]['userID']) {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]['longURL'], email: fetchEmailById(req.session.user_id, users)};
     res.render("urls_show", templateVars);
+  } else if (signInCheck(req)) {
+    res.redirect(urlDatabase[req.params.shortURL]['longURL'])
   } else {
-    res.render("urls_login");
-  } 
+    res.render('urls_alert');
+  }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
 
 app.get("/urls.json", (req, res) => {
   if (signInCheck(req)) {    
     res.json(urlDatabase);
   } else {
-    res.render("urls_login");
+    res.render('urls_alert');
   }
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
+
+app.get ('/public', (req, res) => {  // demo view
+  const urlDBKeys = Object.keys(sampleUrlDB);  //--------------------------------------------------
+  let outPut = {};
+  for (let i of urlDBKeys) {
+    outPut[i] = sampleUrlDB[i]['longURL'];
+  }
+  if (signInCheck(req)) {
+    const templateVars = { urls: outPut, email: fetchEmailById(req.session.user_id, users)};
+    res.render("urls_indexPublic", templateVars);
+  } else {
+    const templateVars = { urls: outPut, email: null};
+    res.render("urls_indexPublic", templateVars);
+  }
+}) 
+//--------------------------------------------------------------------------
+
+app.post('/urlUseCount', (req, res) => {
+  if (signInCheck(req) && req.body.shortURL) { //-------------------------to track url use for analytics-------------------------------
+    urlDatabase[req.body.shortURL]['urlUseCount'] += 1;
+  } else {
+    res.render('urls_alert');
+  }
+})
+
+
+//--------------------------------------------------------------------------
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--------------------------------------------------------------------------
--- helpers.js
+++ helpers.js
@@ -10,6 +10,41 @@ const flatUrlDB = (DB) => { // reduce db to a simple k-v pair object
 
 //--------------------------------------
 
+const querry_DB_By_ID = (id, urlDatabase) => { // to retrieve only data of a particular user id
+  let outPut = {} 
+  const urlDBKeys = Object.keys(urlDatabase);
+  for (let i of urlDBKeys) {
+    if (urlDatabase[i]['userID'] === id) {
+      outPut[i] = urlDatabase[i];
+    }
+  }
+  return flatUrlDB(outPut);
+};
+
+//--------------------------------------
+
+const emailCheck = (email, users) => {
+  const usersdbIDs = Object.keys(users);
+  for (let i of usersdbIDs) {
+    if (users[i]['email'] === email) {
+      return true;
+    } 
+  }
+  return false;
+};
+
+//--------------------------------------
+
+const fetchEmailById = (id, users) => {
+  const usersdbIDs = Object.keys(users);
+  for (let i of usersdbIDs) {
+    if (users[i]['id'] === id) {
+      return users[i]['email'];
+    }
+  }
+}
+//--------------------------------------
+
 function generateRandomString(n) {
   const nums_letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   const alphanumeric = nums_letters.split('');
@@ -23,5 +58,4 @@ function generateRandomString(n) {
 
 //--------------------------------------
 
-
-module.exports = {flatUrlDB, generateRandomString};
+module.exports = {flatUrlDB, generateRandomString, emailCheck, fetchEmailById, querry_DB_By_ID};
